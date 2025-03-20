ff1362b689a42798a1c7f1900a29c6a545d8a4ae
gokulrajrz <rajgokul9847161393@gmail.com>
2025-03-21 00:50:10 +0530

    shadow

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
 
       <!-- Get a Quote Button -->
       <div
-        class="fixed text-white right-5 bottom-5 rounded-[30px] border-2 border-[#ffffff] z-[5]"
+        class="fixed text-white right-5 bottom-5 rounded-[30px] border-2 border-[#ffffff] z-[5] shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
       >
         <button
           @click="isModalOpen = true"
